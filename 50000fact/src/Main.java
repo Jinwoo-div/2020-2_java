b7081e7007326ad2ffae6e1ca5e568dc183cab94
Choi <csw3055@gmail.com>
2020-11-24 23:48:25 +0900

    FEAT 50000factorial caculating

---
+++ 50000fact/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class Factorial {
+private
+	int key;
+	String sum;
+	ArrayList<Character> num = new ArrayList<Character>;
+public
+	Factorial(int key) {
+		this.key = key;
+		sum = Integer.toString(this.key);
+		
+	}
+	void getCaculate() {
+		for (int i = key; i < 0; i--) {
+			num = sum.toCharArray();
+		}
+		
+	}
+			
+}
