af5af2f8c52350ba3f8c8619d2d109d3ad2ab007
Choi <csw3055@gmail.com>
2020-12-15 22:19:56 +0900

    FEAT tetris working

--- tetris/src/tetris/Main.java
+++ tetris/src/tetris/Main.java
@@ -1,5 +1,14 @@
 package tetris;
+import java.util.Random;
+
+import tetris.Ui;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+	}
+	public void createBlock() {
+		Random rand = new Random();
+		rand.setSeed(System.currentTimeMillis());
+	}
 }
--- tetris/src/tetris/Ui.java
+++ tetris/src/tetris/Ui.java
@@ -6,6 +6,18 @@ import javax.swing.JFrame;
 import java.awt.GridLayout;
 import javax.swing.JLabel;
 import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Ui {
 
@@ -39,18 +51,52 @@ public class Ui {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 429, 612);
+		frame.setBounds(100, 100, 666, 708);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(0, 3, 0, 0));
+		Border gameLine = BorderFactory.createLineBorder(Color.black);
+		frame.getContentPane().setLayout(new GridLayout(0, 2, 0, 0));
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		frame.getContentPane().add(lblNewLabel_1);
+		JPanel game = new JPanel();
+		frame.getContentPane().add(game);
+		game.setLayout(new GridLayout(20, 10, 0, 0));
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 10; j++) {
+				JLabel tmp = new JLabel("");
+				tmp.setBorder(gameLine);
+				game.add(tmp);
+				tmp.setName((i+1) + "_" + (j+1));
+			}
+		}
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		frame.getContentPane().add(lblNewLabel);
+		JPanel info = new JPanel();
+		frame.getContentPane().add(info);
+		info.setLayout(null);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		frame.getContentPane().add(lblNewLabel_2);
+		JLabel HighScore = new JLabel("0000");
+		HighScore.setBounds(0, 155, 326, 136);
+		info.add(HighScore);
+		
+		JLabel Score = new JLabel("0000");
+		Score.setBounds(0, 326, 326, 136);
+		info.add(Score);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu fileTab = new JMenu("파일");
+		menuBar.add(fileTab);
+		
+		JMenuItem saveGame = new JMenuItem("저장");
+		fileTab.add(saveGame);
+		
+		JMenuItem loadGame = new JMenuItem("불러오기");
+		fileTab.add(loadGame);
+		
+		JMenuItem endGame = new JMenuItem("게임종료");
+		fileTab.add(endGame);
+		
+		JMenu settingTab = new JMenu("설정");
+		menuBar.add(settingTab);
+		
 	}
-
 }
